bc1296969069faa7ea4f792dad558b8c93b4cbec
herbert1958 <herbertwein@icloud.com>
2022-02-06 14:33:32 +0100

    revised battery calculation

--- test/vsystem/vdisplay/graph.tcl
+++ test/vsystem/vdisplay/graph.tcl
@@ -38,7 +38,7 @@ set h1 [expr {int($h * 0.5)}]  ;# canvas mid-height
 set h2 [expr {$h1 + 1}]
 set h3 [expr {int($h * 0.4)}]  ;# graph mid-height
 # canvas & bindings
-canvas .c -width $params(width) -height $h -xscrollincrement 1 -bg beige
+canvas .c -width $params(width) -height $h -xscrollincrement 1 -bg black
 bind . <Destroy> { exit }
 # plotting
 wm title . $params(title)
@@ -54,11 +54,11 @@ while TRUE {
                 if {abs($vv) < $params(accuracy)} \
                 { 
                                 #puts "$vv [expr {$params(accuracy)}]" ;#because I wanted to understand the points
-                                .c create text $t 0 -anchor n -text [expr { $t / 50. }] -fill gray
-                                .c create line $t 0 $t $h -fill gray
+                                .c create text $t 0 -anchor n -text [expr { $t / 50. }] -fill green
+                                .c create line $t 0 $t $h -fill green
                 }
-                .c create line $t $h1 $t $h2 -fill gray
-                .c create rectangle $t $v $t $v
+                .c create line $t $h1 $t $h2 -fill green
+                .c create rectangle $t $v $t $v -outline green
                 incr t
                 if {$t > $params(width)} { .c xview scroll 1 unit }
         }
