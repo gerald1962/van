b6b631d47ee588a0f0c9edaf369e3bfbff248521
Gerald Schüller <gerald.schueller@web.de>
2022-05-04 18:52:15 +0200

    cweb

---
+++ cweb/examples/hello.c
@@ -0,0 +1,24 @@
+/*1:*/
+#line 7 "hello.w"
+
+/*2:*/
+#line 14 "hello.w"
+
+#include <stdio.h> 
+
+/*:2*/
+#line 8 "hello.w"
+
+/*3:*/
+#line 19 "hello.w"
+
+void main(void)
+{
+printf("Hello\n");
+}
+
+/*:3*/
+#line 9 "hello.w"
+
+
+/*:1*/
