b06c6cbc97800ba19a9d414fb73559a1050e1362
Gerald Schüller <gerald.schueller@web.de>
2022-06-05 16:01:28 +0200

    cwebe

--- cweb/exercise/lip.c
+++
@@ -1,8 +0,0 @@
-% SPDX-License-Identifier: GPL-2.0
-
-% Copyright (C) 2022 Gerald Schueller <gerald.schueller@web.de>
-	      
-% This file is part of LIP - Literate Programming.
-% This program by Gerald Schueller is based on the CWEB
-% program by Silvio Levy and Donald E. Knuth.
-	      
